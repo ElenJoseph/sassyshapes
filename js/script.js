ef4bb97dfcc74afbb5d1f77a1a39ffbecf5e6706
Elen Joseph <youremailaddress@algonquinlive.comjose0205@algonquinlive.com>
2019-11-26 15:20:38 -0500

    sassy shapes started

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 const colors = ['red', 'blue', 'green', 'purple', 'orange']
 const shapes = ['square', 'circle', 'pentagon', 'hexagon', 'octagon']
+
+const colors
+
+colors.inner.HTML = (`<main id="shapes" class="shapes">
+<div class="square-red"></div> 
+<div class="square-blue"></div> 
+<div class="square-green"></div> 
+<div class="square-purple"></div> 
+<div class="square-orange"></div> 
+<div class="circle-red"></div> 
+<div class="circle-blue"></div> 
+...
+<div class="octagon-green"></div> 
+<div class="octagon-purple"></div> 
+<div class="octagon-orange"></div>
+</main>
+`)
