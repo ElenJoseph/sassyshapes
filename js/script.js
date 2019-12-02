695542a55ca12ef5931c6a8dbe1cc2ff034cc726
Elen Joseph <youremailaddress@algonquinlive.comjose0205@algonquinlive.com>
2019-12-02 17:10:32 -0500

    shapes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 const colors = ['red', 'blue', 'green', 'purple', 'orange']
 const shapes = ['square', 'circle', 'pentagon', 'hexagon', 'octagon']
 
-const colors
+const shapeColor = []
+const $shapes = document.getElementById('shapes')
 
-colors.inner.HTML = (`<main id="shapes" class="shapes">
-<div class="square-red"></div> 
-<div class="square-blue"></div> 
-<div class="square-green"></div> 
-<div class="square-purple"></div> 
-<div class="square-orange"></div> 
-<div class="circle-red"></div> 
-<div class="circle-blue"></div> 
-...
-<div class="octagon-green"></div> 
-<div class="octagon-purple"></div> 
-<div class="octagon-orange"></div>
-</main>
-`)
+for (const shape of shapes){
+    for (const color of colors){
+        shapeColor.push(`<div class="${shape}-${color}"></div>`)
+    }
+}
+
+$shapes.innerHTML = shapeColor.join('')
